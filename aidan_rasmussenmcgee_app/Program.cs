fd2a467e16c51b79a40e858b210c01274b756f2f
Aidan Rasmussen McGee <aidan.rasmussenmcgee@umconnect.umt.edu>
2023-09-01 10:48:58 -0600

    First Commit

---
+++ aidan_rasmussenmcgee_app/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+//The below lines are answering the prompts from the syntax-commenting-ice assignment in BMIS 365.
+
+//Answering the "Commenting about me (3 pts.)" section below.
+
+// Aidan Rasmussen McGee
+//Corvallis, MT
+/*
+I love Thai yellow curry with peanuts, mostly anything ethnic is great food.
+Besides homework, my favorite activity is reading fantasy novels and web serials.
+MIS seems to be a great blend between data, business, programming, and the future. I believe it will provide enough of a foundation for me to enter the working field and further my career.
+*/
+
+//Answering the "Syntax and sytle (4 pts.)" section below.
+
+//Copy and pasted the code below from homework and modified by the prompts.
+//Indented line 24
+//Added a semicolon at the end of line 22,23, and 27
+//Fixed camelhump for line 27
+
+int x = 20;
+int y = 18;
+
+if (x > y) 
+{
+    Console.WriteLine("Hello? Is this working?");
+}
+
